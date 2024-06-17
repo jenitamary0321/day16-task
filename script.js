cd1719011685faf5cd2d3791b24bf59ecfb1034d
jenitamary0321 <jenitamary0321@gmail.com>
2024-06-17 21:45:34 +0530

    task

---
+++ script.js
@@ -0,0 +1,35 @@
+const countdownElement = document.getElementById('countdown');
+
+setTimeout(() => {
+    countdownElement.textContent = '10';
+    setTimeout(() => {
+        countdownElement.textContent = '9';
+        setTimeout(() => {
+            countdownElement.textContent = '8';
+            setTimeout(() => {
+                countdownElement.textContent = '7';
+                setTimeout(() => {
+                    countdownElement.textContent = '6';
+                    setTimeout(() => {
+                        countdownElement.textContent = '5';
+                        setTimeout(() => {
+                            countdownElement.textContent = '4';
+                            setTimeout(() => {
+                                countdownElement.textContent = '3';
+                                setTimeout(() => {
+                                    countdownElement.textContent = '2';
+                                    setTimeout(() => {
+                                        countdownElement.textContent = '1';
+                                        setTimeout(() => {
+                                            countdownElement.textContent = 'Happy Independence Day';
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
